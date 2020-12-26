3a618447f0f119de6d7f6b517a61adc9f775781f
Harshit Kumar <kumarharshit850@gmail.com>
2020-12-26 15:12:46 +0530

    Final Project

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Form = ({ inputText ,setInputText, todos, setTodos }) => {
+const Form = ({ inputText ,setInputText, todos, setTodos, setStatus }) => {
   // Here i can write Js functions
   const inputTextHandler = (e) => {
     setInputText(e.target.value);
@@ -14,14 +14,18 @@ const Form = ({ inputText ,setInputText, todos, setTodos }) => {
     setInputText("");
   };
 
+  const statusHandler = (e) => {
+    setStatus(e.target.value);
+  }
+
   return (
     <form>
       <input value={inputText} type="text" required className="todo-input" onChange={inputTextHandler} placeholder="Enter Task" />
       <button className="todo-button" type="submit" onClick={submitTodoHandler}>
-        <i class="fas fa-plus-square"></i>
+        <i className="fas fa-plus-square"></i>
       </button>
       <div className="select">
-        <select name="todos" className="filter-todo">
+        <select name="todos" onClick={statusHandler} className="filter-todo">
           <option value="all">All</option>
           <option value="completed">Completed</option>
           <option value="uncompleted">Uncompleted</option>
